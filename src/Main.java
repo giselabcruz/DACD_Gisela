aa5be5805dc98515a9fa130b2fb095a9af67b334
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-09-19 10:56:38 +0100

    6 commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I am in DACD.");
-        System.out.println("Special attention to the differences: Git is the tool. Github allows us to create a remote " +
-                "repository.");
+        System.out.println("Hello, World! I am in DACD. I am taking some notes.");
+        System.out.println("Special attention to the differences: Git is the tool to have control of the versions. " +
+                "Github is a source code hosting that allow us to create local and remote repositories.");
         System.out.println("Take care with commits!!! We have to look for organization in our repository.");
         System.out.println("If we are in one layer that is not the latest commited, we have to look for one new branch.");
         System.out.println("We use merge option to reintroduce changes we need in branchs.");
+
+        System.out.println("___________________________LAB_____________________________");
+
+        System.out.println("/Head/ is a tag that marks the actual version.");
+
+
+
         }
     }
